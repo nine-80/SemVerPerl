4b83bca11030a30c077cc2a7e00d92d37c6df51f
Derek Douglas <derek@nine-80.com>
2022-02-10 18:52:48 -0500

    Remove auto build number code
    Removed the build number code that was based on the difference between a
    tagged commit and current commit. You can now just pass in a build
    number using the -bn or -build-num option.
    
    Also fixed some checks on $help and $use_git_info so that the behaviour is
    what is expected.

--- sv_gen.pl
+++ sv_gen.pl
@@ -39,11 +39,18 @@ sub check_semver_format {
     return 0; # Bad version format
 }
 
+### Version information record
+my %info = (
+    provided_version => q//,
+    pre_release_type => q//,
+    build_type       => q//,
+    build_num        => q//,
+);
+
 ## Generate the version string from the input to the script
 ## 'git rev-parse' for HEAD will be the hash for the last commit
 sub generate_version {
-    my ($provided_version, $pre_release_type, $build_type, $use_git_info) =
-        @_;
+    my ($provided_version, $use_git_info) = @_;
 
     # Check to make sure $provided_version is in the correct format
     if ($provided_version !~ m/^[0-9].*\.[0-9].*\.[0-9].*$/) {
@@ -51,36 +58,35 @@ sub generate_version {
     }
 
     # sub in appropriate separator for provided pre-release info
+    my $pre_release_type = $info{pre_release_type};
     if ($pre_release_type ne q//) {
         $pre_release_type =~ s/^/-/x;
     }
 
     # sub in appropriate separator for provided build type
+    my $build_type = $info{build_type};
     if ($build_type ne q//) {
         $build_type =~ s/^/./x;
     }
 
     my $build_meta = "";
 
-    if (defined $use_git_info) {
-        ### In order for this to work, there must be a tag in the git repo to count from
-        ### These should be version numbers (e.g. added with git tag -a v0.1.0 -m "version comments")
+    if ($use_git_info) {
         # Determine which NUL we redirect stderr to (POSIX is /dev/null, Win32 is NUL)
         my $shell_null = $Config{osname} eq "MSWin32" ? "NUL" : "/dev/null";
-        my $build_number = qx/git describe --match v$provided_version --dirty 2>$shell_null/ or die '[ERROR] git command failed';
 
         # strip the tag name (version number) and suffixed hyphen
-        $build_number =~ s/v$provided_version-//;
-        chomp $build_number;
+        my $build_number = $info{build_num};
+
+        my $build_hash = qx/git rev-parse --short=12 HEAD 2>$shell_null/ or die '[ERROR] git command failed';
+        chomp $build_hash;
 
         if (length($build_number) > 0) {
-            $build_meta = qq/+$build_number/;
-        } else {
-            # This is build number 1 of the new version tag
-            my $build_hash = qx/git rev-parse --short=12 HEAD 2>$shell_null/ or die '[ERROR] git command failed';
-            chomp $build_hash;
-            #$build_type =~ s/./+/x;
-            $build_meta = qq/+1.$build_hash/;
+            $build_meta = qq/+$build_number.$build_hash/;
+        }
+        else {
+            # Not using a build number
+            $build_meta = qq/+$build_hash/;
         }
     }
 
@@ -92,13 +98,6 @@ sub generate_version {
     return $version_string;
 }
 
-### Version information record
-my %info = (
-    provided_version => q//,
-    pre_release_type => q//,
-    build_type       => q//,
-);
-
 ### Option flags
 my $help = 0;
 my $use_git_info = 0;
@@ -108,25 +107,26 @@ GetOptions(
     'help|?'                 => \$help,
     'prt|pre-release-type=s' => \$info{pre_release_type},
     'bt|build-type=s'        => \$info{build_type},
+    'bn|build-num=i'         => \$info{build_num},
     'gi|use-git-info'        => \$use_git_info
 ) or pod2usage(2);
 
 pod2usage(2) if $help;
 pod2usage(2) if !$ARGV[0];
 
-print generate_version($ARGV[0], $info{pre_release_type}, $info{build_type},
-    $use_git_info);
+print generate_version($ARGV[0], $use_git_info);
 
 __END__
 
 =head1 SYNOPSIS
 
-sv_gen.pl [options] [major.minor.patch]
+sv_gen.pl [options] major.minor.patch
 
  Options:
    -help                            Brief help message
    -prt|pre-release-type <string>   Pre-release information (e.g. dev, alpha, beta, rc)
    -bt|build-type <string>          Build type (e.g. Debug, Release)
-   -gi|use-git-info                 Use Git information (build number & hash) in version string
+   -bn|build-num <int>              Build number
+   -gi|use-git-info                 Use Git information (commit short hash) in version string
 
 =cut
