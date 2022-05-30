54272edce5e7b0d5af0a22fddd0d3284c770f217
Derek Douglas <derek@nine-80.com>
2022-05-30 13:17:53 -0400

    Update header comment to fix LICENSE

--- sv_gen.pl
+++ sv_gen.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 # This file is subject to the terms and conditions defined in
-# file 'LICENSE.md', found in the root of this repository.
+# file, 'LICENSE'
 #
 # Semantic version (semver.org) generation and validation
 #
